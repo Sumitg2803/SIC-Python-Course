1c634b3c435ead29b83f44bd51eeae7bd666d6bd
Sumitg2803 <sumit.s.gaikwad28@gmail.com>
2026-01-06 09:25:45 +0530

    minor update for easy understanding

--- 6 (05-01)/over loading n riding.py
+++ 6 (05-01)/over loading n riding.py
@@ -13,18 +13,20 @@ t.show(10,20) #valid - always the nearest function from the class
 
 #overriding method
 '''
-Child class can override the method of the parent class
-To change the behaviour of parent class method in child class
-To achieve runtime polymorphism
-To provide specific implementation of method in child class
+**What is method overriding?
+    *Overriding is a feature of OOPs where child class can override the method of the parent class
+    *Child class can override the method of the parent class
+    *To change the behaviour of parent class method in child class
+    *To achieve runtime polymorphism
+    *To provide specific implementation of method in child class
 
 **Basic rule of method overriding
-    1. Method name should be same
-    2. Parameters should be same
-    3. Inheritance should be there
-    4. Child method has the priority
-    5. Method signature should be same
-    6. Method body should be different
+    *1. Method name should be same
+    *2. Parameters should be same
+    *3. Inheritance should be there
+    *4. Child method has the priority
+    *5. Method signature should be same
+    *6. Method body should be different
 '''
 class test:
     def show(self, a):
